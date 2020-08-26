714d1cd5408ac6bb8af5f5afad3f36b3aeb37532
kilok <277877130@qq.com>
2020-08-26 15:22:05 +0800

    点击单元测试

--- src/main.js
+++ src/main.js
@@ -19,28 +19,83 @@ const expect = chai.expect
 //单元测试
 {
   const Constructor = Vue.extend(Button)
-  const button = new Constructor({
+  const vm = new Constructor({
     propsData:{
       icon:'settings',
 
     }
   })
-  button.$mount()
-  let userElement = button.$el.querySelector('use')
+  vm.$mount()
+  let userElement = vm.$el.querySelector('use')
   let href = userElement.getAttribute('xlink:href')
   expect(href).to.eq('#i-settings')
+  vm.$el.remove()
+  vm.$destroy()
 }
 
 {
   const Constructor = Vue.extend(Button)
-  const button = new Constructor({
+  const vm = new Constructor({
     propsData:{
       icon:'settings',
       loading:true
     }
   })
-  button.$mount()
-  let userElement = button.$el.querySelector('use')
+  vm.$mount()
+  let userElement = vm.$el.querySelector('use')
   let href = userElement.getAttribute('xlink:href')
   expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const div =document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'settings',
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()
+  vm.$destroy()
+}
+{
+  const div =document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'settings',
+      iconPosition:'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  //mock
+
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'settings',
+    }
+  })
+  vm.$mount()
+  vm.$on('click',function(){
+    console.log(1)
+  })
+  let button = vm.$el
+  button.click()
 }
