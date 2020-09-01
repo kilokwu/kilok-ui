7417b057c83895ed955c9f02eaac33004b2d7da1
kilok <277877130@qq.com>
2020-09-01 19:01:46 +0800

    实现了只能由一个 toast 的功能

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,29 @@
   <div id="app">
     <div>
       <button @click="showToast">点我</button>
-      
     </div>
-   
   </div>
 </template>
 <style lang="scss" scoped>
-
-.sider{
+.sider {
   background: #333;
   width: 150px;
 }
-.sider.slide-enter, .sider.slide-leave-to{
+.sider.slide-enter,
+.sider.slide-leave-to {
   margin-left: -150px;
 }
-.header{
+.header {
   height: 100px;
   background: #999;
 }
-.footer{
+.footer {
   height: 100px;
   background: #999;
 }
 .box {
   margin: 20px;
 }
-
 </style>
 <script>
 export default {
@@ -36,16 +33,27 @@ export default {
       loading1: false,
       loading2: false,
       loading3: false,
-      message:'hi',
+      message: "hi",
     };
   },
   methods: {
     inputChange(e) {
       console.log(e.target.value);
     },
-    showToast(){
-      this.$toast('<strong>hi</strong>',{enableHtml:true})
-}
+    showToast() {
+      this.$toast("欢迎光临", {
+        position: "middle",
+        enableHtml: false,
+        closeButton: {
+          text: "已经欢迎",
+          callback() {
+            console.log("他说欢迎光临");
+          },
+        },
+        autoClose: false,
+        autoCloseDelay: 3,
+      });
+    },
   },
 };
 </script>
--- src/components/toast/toast.vue
+++ src/components/toast/toast.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="toast" ref="wrapper">
+  <div class="toast" ref="wrapper" :class="toastClasses">
     <div class="message">
       <slot v-if="!enableHtml"></slot>
       <div v-else v-html="$slots.default[0]"></div>
@@ -37,10 +37,23 @@ export default {
       type: Boolean,
       default: false,
     },
+    position:{
+        type:String,
+        default:'top',
+        validator(value){
+            return ['top','bottom','middle'].indexOf(value) >=0
+        }
+    }
+  },
+  computed:{
+      toastClasses(){
+          return {[`position-${this.position}`]:true}
+      }
   },
   mounted() {
     this.execAutoClose();
     this.updateStyle();
+
   },
   methods: {
     close() {
@@ -49,7 +62,7 @@ export default {
     },
     onClickClose() {
       this.close();
-    //   this.closeButton.callback();
+      this.closeButton.callback();
     },
     execAutoClose() {
       if (this.autoClose) {
@@ -72,33 +85,35 @@ export default {
 
 <style lang="scss" scoped>
 $font-size: 14px;
-$toast-height: 40px;
+$toast-min-height: 40px;
 $toast-bg: rgba(0, 0, 0, 0.74);
 .toast {
-  position: fixed;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  font-size: $font-size;
-  line-height: 1.8;
-  height: $toast-height;
-  display: flex;
-  align-items: center;
-  background: $toast-bg;
-  border-radius: 4px;
-  box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.5);
-  color: white;
-  padding: 0px 16px;
+  position: fixed;  left: 50%;  font-size: $font-size; line-height: 1.8;
+  min-height: $toast-min-height; display: flex; align-items: center; background: $toast-bg; border-radius: 4px;
+  box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.5); color: white; padding: 0px 16px;
   .line {
     border-left: 1px solid #666;
     margin-left: 16px;
+
   }
   .close {
     padding-left: 16px;
     flex-shrink: 0;
   }
+  &.position-top{
+      top:0;
+      transform:translateX(-50%) ;
+  }
+  &.position-bottom{
+      bottom: 0;
+      transform:translateX(-50%) ;
+  }
+  &.position-middle{
+      top: 50%;
+      transform:translate(-50%,-50%) ;
+  }
 }
 .message{
-    padding:8px 16px
+    padding:8px 16px;
 }
 </style>
