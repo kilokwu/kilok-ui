d76a676a75f0f21392fe66bb007421b8ec2f3d6d
kilok <277877130@qq.com>
2020-09-04 20:34:22 +0800

    重构popover

--- src/App.vue
+++ src/App.vue
@@ -17,39 +17,55 @@
         <k-tabs-pane name="sports">体育相关信息</k-tabs-pane>
       </k-tabs-body>
     </k-tabs> -->
-    <div class="yy">
+    <div class="yy" @click="yyy">
+      <k-popover >
+        <template slot="content">
+          <div>popover内容</div>
+        </template>
+        <template>
+          <button>点我</button>
+        </template>
+      </k-popover>
 
-        <k-popover>
-      <template slot="content">
-        <div>popover内容</div>
-      </template>
-      <template >
-        <button >点我</button>
-      </template>
-    </k-popover>
-  
-    <k-popover>
-      <template slot="content">
-        <div>popover内容2</div>
-      </template>
-      <template >
-        <button >点我</button>
-      </template>
-    </k-popover>
+      <k-popover>
+        <template slot="content">
+          <div>popover内容2</div>
+        </template>
+        <template>
+          <button>点我</button>
+        </template>
+      </k-popover>
     </div>
-  
-
   </div>
 </template>
 <style lang="scss" scoped>
-.sider { background: #333; width: 150px; } .sider.slide-enter, .sider.slide-leave-to { margin-left: -150px; } .header { height: 100px; background: #999; } .footer { height: 100px; background: #999; } .box { margin: 20px; }
-.xx{
+.sider {
+  background: #333;
+  width: 150px;
+}
+.sider.slide-enter,
+.sider.slide-leave-to {
+  margin-left: -150px;
+}
+.header {
+  height: 100px;
+  background: #999;
+}
+.footer {
+  height: 100px;
+  background: #999;
+}
+.box {
+  margin: 20px;
+}
+.xx {
   padding-top: 100px;
   padding-left: 100px;
-  border:1px solid red;
+  border: 1px solid red;
 }
-.yy{
+.yy {
   border: 1px solid green;
+  overflow: hidden;
 }
 </style>
 <script>
@@ -60,13 +76,13 @@ export default {
       loading2: false,
       loading3: false,
       message: "hi",
-      selectedTab:'sports'
+      selectedTab: "sports",
     };
   },
   methods: {
     showToast() {
       this.$toast("欢迎光临", {
-        position:"top",
+        position: "top",
         enableHtml: false,
         closeButton: {
           text: "已经欢迎",
@@ -77,10 +93,9 @@ export default {
         autoClose: 3,
       });
     },
-  yyy(data){
-    console.log('yyy')  
-    console.log(data)
-  }
+    yyy() {
+      console.log("yyy");
+    },
   },
 };
 </script>
--- src/components/popover/popover.vue
+++ src/components/popover/popover.vue
@@ -1,15 +1,10 @@
 <template>
-  <div class="k-popover" @click.stop="xxx">
-    <div
-      ref="contentWrapper"
-      v-if="visible"
-      class="content-wrapper"
-      @click="zzz"
-    >
+  <div class="k-popover" @click="onClick" ref="popover">
+    <div ref="contentWrapper" v-if="visible" class="content-wrapper">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
-    <slot ></slot>
+      <slot></slot>
     </span>
   </div>
 </template>
@@ -23,33 +18,41 @@ export default {
     };
   },
   methods: {
-    zzz(){
-      console.log('zzz')
-    }
-    ,
-    xxx() {
-      this.visible = !this.visible;
-      if (this.visible === true) {
-        this.$nextTick(() => {
-            document.body.appendChild(this.$refs.contentWrapper);
-            let {top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
-            console.log(top,left)
-            this.$refs.contentWrapper.style.left = left+window.scrollY+'px'
-            this.$refs.contentWrapper.style.top = top+window.scrollY+'px'
-          let eventHandler = () => {
-            console.log("document 隐藏 popover");
-            this.visible = false;
-            document.removeEventListener("click", eventHandler);
-          };
-          document.addEventListener("click", eventHandler);
-        });
-      } else {
-        console.log("vm 隐藏 popover");
+    positionContent() {
+      document.body.appendChild(this.$refs.contentWrapper);
+      let { top, left } = this.$refs.triggerWrapper.getBoundingClientRect();
+      this.$refs.contentWrapper.style.left = left + window.scrollX + "px";
+      this.$refs.contentWrapper.style.top = top + window.scrollY + "px";
+    },
+    onClickDocument(e){
+      if ( this.$refs.popover && (this.$refs.popover ===e.target || this.$refs.popover.contains(e.target)) ) {
+        return;
+       }
+      this.close();
+    },
+
+    open() {
+      this.visible = true;
+      setTimeout(() => {
+        this.positionContent();
+        document.addEventListener("click", this.onClickDocument);
+      }, 0);
+    },
+    close() {
+      this.visible = false;
+      document.removeEventListener("click", this.onClickDocument);
+    },
+    onClick(event) {
+      if (this.$refs.triggerWrapper.contains(event.target)) {
+        if (this.visible === true) {
+          this.close();
+        } else {
+          this.open();
+        }
       }
     },
   },
   mounted() {
-    console.log(this.$refs.triggerWrapper)
   },
 };
 </script>
@@ -59,10 +62,10 @@ export default {
   vertical-align: top;
   position: relative;
 }
-  .content-wrapper {
-    position: absolute;
-    border: 1px solide red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    transform: translateY(-100%);
-  }
+.content-wrapper {
+  position: absolute;
+  border: 1px solide red;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  transform: translateY(-100%);
+}
 </style>
