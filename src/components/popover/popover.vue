552abe76ac57334763f112dcde5d562282fd818a
kilok <277877130@qq.com>
2020-09-04 23:20:56 +0800

    使用表驱动变成重构代码

--- src/components/popover/popover.vue
+++ src/components/popover/popover.vue
@@ -36,26 +36,21 @@ export default {
       const { contentWrapper, triggerWrapper } = this.$refs;
       document.body.appendChild(contentWrapper);
       let { top, left, width, height } = triggerWrapper.getBoundingClientRect();
-      console.log(width);
-      if (this.position === "top") {
-        contentWrapper.style.left = left + window.scrollX + "px";
-        contentWrapper.style.top = top + window.scrollY + "px";
-      } else if (this.position === "bottom") {
-        contentWrapper.style.left = left + window.scrollX + "px";
-        contentWrapper.style.top = top + height + window.scrollY + "px";
-      } else if (this.position === "left") {
-        contentWrapper.style.left = left + window.scrollX + "px";
-        contentWrapper.style.top = top + window.scrollY + "px";
-        let { height: height2 } = contentWrapper.getBoundingClientRect();
-        contentWrapper.style.top =
-          top + window.scrollY + (height - height2) / 2 + "px";
-      } else if (this.position === "right") {
-        contentWrapper.style.left = left + width + window.scrollX + "px";
-        contentWrapper.style.top = top + window.scrollY + "px";
-        let { height: height2 } = contentWrapper.getBoundingClientRect();
-        contentWrapper.style.top =
-          top + window.scrollY + (height - height2) / 2 + "px";
+      let { height: height2 } = contentWrapper.getBoundingClientRect();
+      let positions = {
+        top:{ left: left + window.scrollX, top:top + window.scrollY },
+        bottom:{ left:left + window.scrollX, top:top + height + window.scrollY },
+        left:{
+          left:left + window.scrollX ,
+          top: top + window.scrollY + (height - height2) / 2
+        },
+        right:{
+          left:left + width + window.scrollX,
+          right:top + window.scrollY + (height - height2) / 2
+        }
       }
+      contentWrapper.style.left = x[this.position].left+'px'
+      contentWrapper.style.top = x[this.position].top+'px'
     },
     onClickDocument(e) {
       if (
