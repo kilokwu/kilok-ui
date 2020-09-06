97e9a9cb6d7aadaf090a29a77b7cfbc27c01348e
kilok <277877130@qq.com>
2020-09-06 16:04:02 +0800

    更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   description: "一个好用的UI框架",
   themeConfig: {
     nav: [
-      { text: "主页", link: "/" },
+      { text: "主页", link: "/" }, 
       { text: "文档", link: "/guide" },
       { text: "交流", link: "/https://google.com" },
     ],
